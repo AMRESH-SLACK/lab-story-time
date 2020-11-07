c388b3612acf9e426f9906f21c922cb827a7ebc5
AMRESH-SLACK <67509274+AMRESH-SLACK@users.noreply.github.com>
2020-11-07 19:00:03 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,14 +3,33 @@
 //Debug the type of data provided
 //Return the types concatenated in a single variable
 
+function moreAboutHome(address, distanceFromTown, hasNeighbours) {
+    var result = typeof address + typeof distanceFromTown + typeof hasNeighbours;
+    return result;
+}
+
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
+
+function moreAboutKaren(parents, noOfSiblings, isNuclearFamily) {
+    if(typeof(parents) == "string" &&  typeof(noOfSiblings)== "number" && typeof(isNuclearFamily)=="boolean") {
+        return true
+    }
+    return false
+}
 
 //Progression 3:
 //Lily is suspicious about Karen's new friend
 //Karen tells her friend's age and even writes it down
 //Check which one those is not a number (NaN) and return that value
+
+function doesFriendExist(ageInText, ageInNumber) {
+     if(isNaN(ageInText)) {
+         return ageInText
+     }
+     return ageInNumber
+}
 
 //Progression 4:
 //Lily gave Karen x sweets
@@ -19,9 +38,20 @@
 //Her friend divided the remaining sweets into 2 parts for each
 //How many sweets did her friend get to eat?
 
+function sweetTooth(totalNoOfSweets, sweetsConsumedByKaren, sweetsConsumedInNMeters, metersToTravel) {
+    var remaining = totalNoOfSweets - (sweetsConsumedByKaren + sweetsConsumedInNMeters * metersToTravel)
+    remaining = remaining / 2
+    return remaining
+}
+
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
 //It only shows in fahrenheit. Convert the data to celsius and return it.
+
+function convertToCelsius(fahrenheit) {
+    var celsius = (fahrenheit - 32) * (5 / 9)
+    return celsius
+}
 
 //Progression 6:
 //Lily can now do multiple things to deal with this
@@ -31,6 +61,22 @@
 //4. Lock her daughter in her room
 //Given a value, return which of these above actions Lily would take
 
+function aDifficultChoice(choice) {
+  switch(choice) {
+      case 1 : return "Take her daughter to a doctor"
+      break;
+      case -1 : return "Break down and give up all hope"
+      break;
+      case 2 : return "Talk to her husband about it"
+      break;
+      case 3 : return "Counsel her daughter herself"
+      break;
+      case 4 : return "Lock her daughter in her room"
+      break;
+      default : return "Wasn't able to decide"
+  }
+}
+
 //Progression 7:
 //Lily realized that she'd hurt her daughter
 //All she wants now is for her to stop crying
@@ -39,3 +85,5 @@
 //Take all of Lily's strategies and concatenate them to a single var
 //Seperate the strategies by a single space
 //Return the length of the complete strategy
+
+
